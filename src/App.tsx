d56774bcdd78d4f8e42f531e99ab8a688f7fcdeb
Daniel Azevedo <saviodaniel1234@gmail.com>
2025-10-15 00:09:41 +0100

    Mensagens de notificação com toastfy

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { Home } from './pages/Home';
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 import './styles/global.css';
 import './styles/theme.css';
+import { MessagesContainer } from './components/MessagesContainer';
 
 
 export function App(){
 
     return(
         <TaskContextProvider>
+            <MessagesContainer>
             <Home />
+            </MessagesContainer>
         </TaskContextProvider>
     );
 }
